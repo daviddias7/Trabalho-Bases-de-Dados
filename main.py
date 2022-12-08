ab20cb429cd772cd2b0884264ffab9c01152833e
VitorFCM <vitor.carreramiguel@gmail.com>
2022-12-08 19:38:19 -0300

    Realizando o try para se conectar ao banco

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import cx_Oracle
 import config
 
-con = cx_Oracle.connect(config.username, config.password, config.dsn, encoding=config.encoding)
+con = None
+try:
+
+    con = cx_Oracle.connect(config.username, config.password, config.dsn, encoding=config.encoding)
+
+    print("Conexao com o banco de dados estabelecida.")
+
+except cx_Oracle.DatabaseError as e:
+    print("Ocorreu um problema ao se conectar com o banco de dados: ", e)
+
 
 print(con)
 
