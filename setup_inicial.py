4b20558684f0a3229394103acc7477639d187ac3
VitorFCM <vitor.carreramiguel@gmail.com>
2022-12-09 08:52:10 -0300

    Apenas testando coisas

--- setup_inicial.py
+++ setup_inicial.py
@@ -7,16 +7,47 @@ import config
 
 con = cx_Oracle.connect(config.username, config.password, config.dsn, encoding=config.encoding)
 
-print(con)
+#print(con)
 
 cursor = con.cursor()
+cursor.execute('SELECT * FROM PESSOA')
+print(cursor.fetchall()[1])
 
-with open("esquema.sql", "r") as file:
-    commands = file.read().split(";")
-    print(commands)
-    commands.pop()
-    for command in commands:
-        command = command[command.find('C'):]
-        print("Executando comando: ", command.split("\n")[0], " ...")
-        cursor.execute(command)
+#cursor.execute("DROP TABLE RELACIONA")
+#cursor.execute("DROP TABLE AVISTAMENTO")
+#cursor.execute("DROP TABLE DOACAO")
+#cursor.execute("DROP TABLE VETERINARIO")
+#cursor.execute("DROP TABLE GATO_LIVRE")
+#cursor.execute("DROP TABLE GATO_RESGATADO")
+#cursor.execute("DROP TABLE GATO_ADOTADO")
+#cursor.execute("DROP TABLE GATO")
+#cursor.execute("DROP TABLE ADOTANTE")
+#cursor.execute("DROP TABLE DOADOR")
+#cursor.execute("DROP TABLE ABRIGO_TEMPORARIO")
+#cursor.execute("DROP TABLE GASTOS")
+#cursor.execute("DROP TABLE MEMBRO")
+#cursor.execute("DROP TABLE PESSOA")
 
+
+#with open("esquema.sql", "r") as file:
+#    commands = file.read().split(";")
+#    commands.pop()
+#    for command in commands:
+#        command = command[command.find('C'):]
+#        print("Executando comando: ", command.split("\n")[0], " ...")
+#        cursor.execute(command)
+
+#with open("dados.sql", "r") as file:
+#    data = file.read().split(";")
+#    data.pop()
+
+    #print(data[len(data) - 1])
+    #cursor.execute(data[len(data) - 1])
+#    for d in data:
+#        d = d[d.find('I'):]
+#        print("Executando comando: ", d)
+#        cursor.execute(d)
+
+#con.commit()
+cursor.close()
+con.close()
