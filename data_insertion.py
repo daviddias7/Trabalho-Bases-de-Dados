a0051ee36e0eeb9b9bebce94514f1f09263ac41e
VitorFCM <vitor.carreramiguel@gmail.com>
2022-12-09 20:58:37 -0300

    Todas as classes das tabelas criadas e criacao do arquivo regex

--- data_insertion.py
+++ data_insertion.py
@@ -1,5 +1,6 @@
 import re
 import cx_Oracle
+import regex_checks
 
 class Atribute:
     def __init__(self, name, isNullable, pType, regex, possible_values, applyUpper):
@@ -31,7 +32,7 @@ class Atribute:
 
             if(value == ""):
                 if(self.isNullable):
-                    return "NULL"
+                    return None
                 else:
                     print(self.name + " nao pode ser um valor nulo.")
                     continue
@@ -61,10 +62,10 @@ class Atribute:
             else:
                 if(self.applyUpper): value = value.upper()
                 if(self.value_is_possible(value)):
-                    if(self.regex != ""):
-                        check = re.search(self.regex, value)
+                    if(self.regex[0] != ""):
+                        check = re.search(self.regex[0], value)
                         if not check:
-                            print("O valor digitado nao esta no padrao especificado")
+                            print("O valor digitado precisa " + self.regex[1])
                             continue
                     return value
                 else:
@@ -75,37 +76,22 @@ class Atribute:
 
 class Table:
 
-    def __init__(self):
-        self.atributes = []
+    def __init__(self, atributes, sql_insert, table_name):
+        self.atributes = atributes
+        self.sql_insert = sql_insert
+        self.table_name = table_name
 
-    def insert_data(self):
-        atribute_values = []
-        for atribute in self.atributes:
-            atribute_values.append(atribute.get_value())
-
-        return atribute_values
-
-
-class Pessoa(Table):
-
-    def __init__(self):
-        super().__init__()
-        self.atributes = [
-                Atribute("CPF", False, "string", "[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}", [], False),
-                Atribute("NOME", False, "string", "^[a-zA-Z ]{1,20}$", [], True)
-                ]
     def insert_data(self, connection):
-        sql = """INSERT INTO PESSOA (CPF, NOME)
-          VALUES (:P_CPF, :P_NOME)"""
-
         cursor = connection.cursor()
 
         while(True):
-            data = super().insert_data()
-            print("Tentando inserir o registro " + str(data) + " na tabela Pessoa")
+            atribute_values = []
+            for atribute in self.atributes:
+                atribute_values.append(atribute.get_value())
+            print("Tentando inserir o registro " + str(atribute_values) + " na tabela " + self.table_name)
 
             try:
-                cursor.execute(sql, data)
+                cursor.execute(self.sql_insert, atribute_values)
                 connection.commit()
                 print("Sucesso ao inserir o registro!")
                 return
@@ -114,59 +100,244 @@ class Pessoa(Table):
                 print("Por favor, verifique o erro e insira os dados novamente.")
 
 
+class Pessoa(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("CPF", False, "string", regex_checks.cpf, [], False),
+                Atribute("NOME", False, "string", regex_checks.max_string(20), [], True)
+                ]
+        sql_insert = """INSERT INTO PESSOA (CPF, NOME)
+          VALUES (:P_CPF, :P_NOME)"""
+
+        table_name = "PESSOA"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+
 class Membro(Table):
 
     def __init__(self):
-        super().__init__()
-        self.atributes = [
-                Atribute("CPF", False, "string", "[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}", [], False),
-                Atribute("FUNCAO", False, "string", "", ['FINANCEIRO', 'MARKETING'], True)
+        atributes = [
+                Atribute("CPF", False, "string", regex_checks.cpf, [], False),
+                Atribute("FUNCAO", False, "string", regex_checks.empty, ['FINANCEIRO', 'MARKETING'], True)
                 ]
+
+        sql_insert = """INSERT INTO MEMBRO (CPF, FUNCAO)
+          VALUES (:M_CPF, :M_FUNCAO)"""
+
+        table_name = "MEMBRO"
+
+        super().__init__(atributes, sql_insert, table_name)
 
 class Doador(Table):
 
     def __init__(self):
-        super().__init__()
-        self.atributes = [
-                Atribute("CPF", False, "string", "[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}", [], False),
+        atributes = [
+                Atribute("CPF", False, "string", regex_checks.cpf, [], False),
                 ]
+
+        sql_insert = """INSERT INTO DOADOR (CPF)
+          VALUES (:D_CPF)"""
+
+        table_name = "DOADOR"
+
+        super().__init__(atributes, sql_insert, table_name)
 
 class Adotante(Table):
 
     def __init__(self):
-        super().__init__()
-        self.atributes = [
-                Atribute("CPF", False, "string", "[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}", [], False),
-                Atribute("RUA", True, "string", "^[a-zA-Z ]{1,50}$", [], True),
+        atributes = [
+                Atribute("CPF", False, "string", regex_checks.cpf, [], False),
+                Atribute("RUA", True, "string", regex_checks.max_string(50), [], True),
                 Atribute("NUMERO", True, "int", "", [], False),
-                Atribute("BAIRRO", True, "string", "^[a-zA-Z ]{1,50}$", [], True),
-                Atribute("CIDADE", True, "string", "^[a-zA-Z ]{1,30}$", [], True)
+                Atribute("BAIRRO", True, "string", regex_checks.max_string(50), [], True),
+                Atribute("CIDADE", True, "string", regex_checks.max_string(30), [], True)
                 ]
+
+        sql_insert = """INSERT INTO DOADOR (CPF, RUA, NUMERO, BAIRRO, CIDADE)
+          VALUES (:A_CPF, :A_RUA, :A_NUMERO, :A_BAIRRO, :A_CIDADE)"""
+
+        table_name = "ADOTANTE"
+
+        super().__init__(atributes, sql_insert, table_name)
 
 class Gastos(Table):
 
     def __init__(self):
-        super().__init__()
-        self.atributes = [
-                Atribute("NOTA_FISCAL", False, "string", "[0-9]{9}", [], False),
-                Atribute("ORIGEM", False, "string", "^[a-zA-Z ]{1,16}$", [], True),
+        atributes = [
+                Atribute("NOTA_FISCAL", False, "string", regex_checks.max_number(9), [], False),
+                Atribute("ORIGEM", False, "string", regex_checks.max_string(16), [], True),
                 Atribute("VALOR_DO_GASTO", False, "float", "", [], False),
-                Atribute("MEMBRO_PAGAMENTO", True, "string", "[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}", [], False),
+                Atribute("MEMBRO_PAGAMENTO", True, "string", regex_checks.cpf, [], False),
                 Atribute("FUNCAO", True, "string", "", ["FINANCEIRO", "MARKETING"], True)
                 ]
+
+        sql_insert = """INSERT INTO GASTOS (NOTA_FISCAL, ORIGEM, VALOR_DO_GASTO, MEMBRO_PAGAMENTO)
+          VALUES (:G_NOTA_FISCAL, :G_ORIGEM, :G_VALOR_DO_GASTO, :G_MEMBRO_PAGAMENTO)"""
+
+        table_name = "GASTOS"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class AbrigoTemporario(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("NOTA_FISCAL", False, "string", regex_checks.max_number(9), [], False),
+                Atribute("RUA", False, "string", regex_checks.max_string(50), [], True),
+                Atribute("NUMERO", False, "int", "", [], False),
+                ]
+
+        sql_insert = """INSERT INTO ABRIGO_TEMPORARIO (NOTA_FISCAL, RUA, NUMERO)
+          VALUES (:A_NOTA_FISCAL, :A_ORIGEM, :A_VALOR_DO_GASTO)"""
+
+        table_name = "ABRIGO_TEMPORARIO"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class Gato(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("NOME", False, "string", regex_checks.max_string(20), [], True),
+                Atribute("RACA", True, "string", regex_checks.max_string(20), [], True),
+                Atribute("CONDICAO", False, "string", regex_checks.max_string(10), ["RESGATADO", "ADOTADO", "LIVRE"], True),
+                Atribute("MEMBRO_ANUNCIANTE", True, "string", regex_checks.cpf, [], False),
+                Atribute("FUNCAO", True, "string", regex_checks.max_string(10), ["FINANCEIRO", "MARKETING"], True),
+                ]
+
+        sql_insert = """INSERT INTO GATO (NOME, RACA, CONDICAO, MEMBRO_ANUNCIANTE, FUNCAO)
+          VALUES (:G_NOME, :G_RACA, :G_CONDICAO, :G_MEMBRO_ANUNCIANTE, :G_FUNCAO)"""
+
+        table_name = "GATO"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class GatoAdotado(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("NOME", False, "string", regex_checks.max_string(20), [], True),
+                Atribute("ADOTANTE", False, "string", regex_checks.cpf, [], False)
+                ]
+
+        sql_insert = """INSERT INTO GATO_ADOTADO (NOME, ADOTANTE)
+          VALUES (:G_NOME, :G_ADOTANTE)"""
+
+        table_name = "GATO_ADOTADO"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class GatoResgatado(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("NOME", False, "string", regex_checks.max_string(20), [], True),
+                Atribute("CASTRADO", False, "string", regex_checks.empty, ["Y", "N"], True),
+                Atribute("ABRIGO", False, "string", regex_checks.max_number(9), [], False)
+                ]
+
+        sql_insert = """INSERT INTO GATO_RESGATADO (NOME, CASTRADO, ABRIGO)
+          VALUES (:G_NOME, :G_CASTRADO, :G_ABRIGO)"""
+
+        table_name = "GATO_RESGATADO"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class GatoLivre(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("NOME", False, "string", regex_checks.max_string(20), [], True),
+                ]
+
+        sql_insert = """INSERT INTO GATO_LIVRE (NOME)
+          VALUES (:G_NOME)"""
+
+        table_name = "GATO_LIVRE"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class Veterinario(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("NOTA_FISCAL", False, "string", regex_checks.max_number(9), [], False),
+                Atribute("CRMV", True, "string", regex_checks.max_number(9), [], False),
+                Atribute("NOME", True, "string", regex_checks.max_string(20), [], True),
+                Atribute("GATO", False, "string", regex_checks.max_string(20), [], True),
+                ]
+
+        sql_insert = """INSERT INTO VETERINARIO (NOTA_FISCAL, CRMV, NOME, GATO)
+          VALUES (:V_NOTA_FISCAL, :V_CRMV, :V_NOME, :V_GATO)"""
+
+        table_name = "VETERINARIO"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class Doacao(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("DATAHORA", False, "string", regex_checks.date, [], False),
+                Atribute("CPF_MEMBRO", False, "string", regex_checks.cpf, [], False),
+                Atribute("FUNCAO_MEMBRO", False, "string", regex_checks.empty, ["FINANCEIRO"], True),
+                Atribute("CPF_DOADOR", False, "string", regex_checks.cpf, [], False),
+                Atribute("QUANTIA", False, "float", regex_checks.empty, [], False)
+                ]
+
+        sql_insert = """INSERT INTO DOACAO (DATAHORA, CPF_MEMBRO, FUNCAO_MEMBRO, CPF_DOADOR, QUANTIA)
+          VALUES (:D_DOACAO, :D_CPF_MEMBRO, :D_FUNCAO_MEMBRO, :D_CPF_DOADOR, :D_QUANTIA)"""
+
+        table_name = "DOACAO"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class Avistamento(Table):
+
+    def __init__(self):
+        atributes = [
+                Atribute("DATAHORA", False, "string", regex_checks.date, [], False),
+                Atribute("DESCRICAO_DO_LOCAL", False, "string", regex_checks.max_string(100), [], False),
+                Atribute("PESSOA", True, "string", regex_checks.cpf, [], False)
+                ]
+
+        sql_insert = """INSERT INTO AVISTAMENTO (DATAHORA, DESCRICAO_DO_LOCAL, PESSOA)
+          VALUES (:A_DATAHORA, :A_DESCRICAO_DO_LOCAL, :A_PESSOA)"""
+
+        table_name = "AVISTAMENTO"
+
+        super().__init__(atributes, sql_insert, table_name)
+
+class Relaciona(Table):
+    def __init__(self):
+        atributes = [
+                Atribute("GATO", False, "string", regex_checks.empty, [], True),
+                Atribute("AVISTAMENTO", False, "string", regex_checks.date, [], False),
+                Atribute("MEMBRO", False, "string", regex_checks.cpf, [], False),
+                Atribute("FUNCAO", False, "string", regex_checks.empty, ["FINANCEIRO", "MARKETING"], True),
+                ]
+
+        sql_insert = """INSERT INTO RELACIONA (GATO, AVISTAMENTO, MEMBRO, FUNCAO)
+          VALUES (:R_GATO, :R_AVISTAMENTO, :R_MEMBRO, :R_FUNCAO)"""
+
+        table_name = "RELACIONA"
+
+        super().__init__(atributes, sql_insert, table_name)
 
 tables = [("PESSOA", Pessoa()),
         ("MEMBRO", Membro()),
         ("DOADOR", Doador()),
         ("ADOTANTE", Adotante()),
         ("GASTOS", Gastos()),
-        ("ABRIGO_TEMPORARIO", Pessoa()),
-        ("GATO", Pessoa()),
-        ("GATO_ADOTADO", Pessoa()),
-        ("GATO_RESGATADO", Pessoa()),
-        ("GATO_LIVRE", Pessoa()),
-        ("VETERINARIO", Pessoa()),
-        ("DOACAO", Pessoa()),
-        ("AVISTAMENTO", Pessoa()),
-        ("RELACIONA", Pessoa())]
+        ("ABRIGO_TEMPORARIO", AbrigoTemporario()),
+        ("GATO", Gato()),
+        ("GATO_ADOTADO", GatoAdotado()),
+        ("GATO_RESGATADO", GatoResgatado()),
+        ("GATO_LIVRE", GatoLivre()),
+        ("VETERINARIO", Veterinario()),
+        ("DOACAO", Doacao()),
+        ("AVISTAMENTO", Avistamento()),
+        ("RELACIONA", Relaciona())]
 
---
+++ regex_checks.py
@@ -0,0 +1,13 @@
+cpf = ("[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}", "possuir o formato xxx.xxx.xxx-xx")
+
+empty = ("", "")
+
+date = ("[0-9]{2}\/[0-9]{2}\/[0-9]{4}\ [0-9]{2}\:[0-9]{2}\:[0-9]{2}", "estar no formato YYYY/MM/DD HH24:MI:SS")
+
+def max_number(max_len):
+    max_len = str(max_len)
+    return ("[0-9]{" + max_len + "}", "conter um numero de no maximo " + max_len)
+
+def max_string(max_len):
+    max_len = str(max_len)
+    return ("^[a-zA-Z ]{1," + max_len + "}$", "conter no maximo " + max_len + " caracteres do alfabeto")
